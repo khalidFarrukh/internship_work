9bdfb54c721b84b9b9924e6e3e1cbf765f36b5cf
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-11-16 22:16:45 +0500

    v0.0.1

--- PC/app-1/App.js
+++ PC/app-1/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello, my name is Farrukh khalid</Text>
       <StatusBar style="auto" />
     </View>
   );
