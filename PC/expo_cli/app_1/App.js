9b95b3021c6c5644a9834d0ce5fd10d99550cfc0
Farrukh Khalid <khalidfarrukh951@gmail.com>
2022-11-24 07:10:27 +0500

    v0.0.2

--- PC/expo_cli/app_1/App.js
+++ PC/expo_cli/app_1/App.js
@@ -15,22 +15,9 @@ import Onboarding_1 from "./components/Onboarding_1/Onboarding_1";
 import Onboarding_2 from "./components/Onboarding_2/Onboarding_2";
 import Onboarding_3 from "./components/Onboarding_3/Onboarding_3";
 import Sign_in from "./components/Sign_in/Sign_in";
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  splash_image: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    width: "50%",
-    height: 138,
-    resizeMode: "contain",
-  },
-});
+import Sign_up from "./components/Sign_up/Sign_up";
+import Verification from "./components/Verification/Verification";
 
 export default function App() {
-  return <Sign_in />;
+  return <Verification />;
 }
---
+++ PC/expo_cli/app_1/components/Verification/Verification.js
@@ -0,0 +1,242 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  Animated,
+  Image,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+} from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: "center",
+  },
+  column: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '50%',
+  },
+  bg_tr: {
+    position: "absolute",
+    top: "-50%",
+    right: "-35%",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  bg_dr: {
+    position: "absolute",
+    top: "35%",
+    right: "-65%",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  bg_tl: {
+    position: "absolute",
+    top: "-35%",
+    left: "-35%",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  bg_dl: {
+    position: "absolute",
+    top: "50%",
+    left: "-50%",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  go_back_icon: {
+    position: "absolute",
+    top: "-40%",
+    left: "7%",
+    width: "6%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  title: {
+    position: "absolute",
+    top: "-35%",
+    left: "8%",
+    width: "35%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  title_detail: {
+    position: "absolute",
+    top: "-28.5%",
+    left: "8%",
+    width: "65%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  box_input_field_1: {
+    position: "absolute",
+    top: "-18.5%",
+    left: "11%",
+    width: "14%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  box_input_field_2: {
+    position: "absolute",
+    top: "-18.5%",
+    left: "32%",
+    width: "14%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  box_input_field_3: {
+    position: "absolute",
+    top: "-18.5%",
+    left: "53%",
+    width: "14%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  box_input_field_4: {
+    position: "absolute",
+    top: "-18.5%",
+    left: "74%",
+    width: "14%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+
+  continue_button: {
+    position: "absolute",
+    top: "-5.5%",
+    width: "90%",
+    height: "100%",
+    alignSelf: 'center',
+    resizeMode: "contain",
+  },
+  resend_code_in: {
+    width: "70%",
+    left:"30%",
+    resizeMode: "contain",
+  },
+  text_counter:{
+    fontSize:20,
+    left:"-18%",    
+  },
+  numpad_background: {
+    position: "absolute",
+    backgroundColor: "#D2D5DB",
+    bottom: "-70%",
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  key_button: {
+    position: "absolute",
+    width: "30%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  key_1: {
+    bottom: "-24%",
+    left: "2.5%",
+  },
+  key_2: {
+    bottom: "-24%",
+    left: "35%",
+  },
+  key_3: {
+    bottom: "-24%",
+    right: "2.5%",
+  },
+  key_4: {
+    bottom: "-31%",
+    left: "2.5%",
+  },
+  key_5: {
+    bottom: "-31%",
+    left: "35%",
+  },
+  key_6: {
+    bottom: "-31%",
+    right: "2.5%",
+  },
+  key_7: {
+    bottom: "-38%",
+    left: "2.5%",
+  },
+  key_8: {
+    bottom: "-38%",
+    left: "35%",
+  },
+  key_9: {
+    bottom: "-38%",
+    right: "2.5%",
+  },
+  key_0: {
+    bottom: "-45%",
+    right: "35%",
+  },
+
+
+});
+
+const Verification = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Image style={styles.bg_tr} source={require("./images/b_t_r.png")} />
+        <Image style={styles.bg_dr} source={require("./images/b_d_r.png")} />
+        <Image style={styles.bg_tl} source={require("./images/b_t_l.png")} />
+        <Image style={styles.bg_dl} source={require("./images/b_d_l.png")} />
+        <Image style={styles.go_back_icon} source={require("./images/back.png")} />
+        <Image style={styles.title} source={require("./images/verification.png")} />
+        <Image style={styles.title_detail} source={require("./images/verification_detail.png")} />
+        <Image style={styles.box_input_field_1} source={require("./images/current_field.png")} />
+        <Image style={styles.box_input_field_2} source={require("./images/empty_field.png")} />
+        <Image style={styles.box_input_field_3} source={require("./images/empty_field.png")} />
+        <Image style={styles.box_input_field_4} source={require("./images/empty_field.png")} />
+        <Image style={styles.continue_button} source={require("./images/button_continue.png")} />
+        {/* <View style={styles.view_resend_code_in}>
+          <Image style={styles.resend_code_in} source={require("./images/resend_code_in.png")} />
+          <Text>
+            : 00
+          </Text>
+        </View> */}
+        <View style={styles.row}>
+          <View style={styles.column}>
+            <Image style={styles.resend_code_in} source={require("./images/resend_code_in.png")} />
+          </View>
+          <View style={styles.column}>
+            <Text style= {styles.text_counter}>Sample</Text>
+          </View>
+        </View>
+        {/* <View style={styles.numpad_background} />
+        <Image style={[styles.key_button, styles.key_1]} source={require("./images/1.png")} />
+        <Image style={[styles.key_button, styles.key_2]} source={require("./images/2.png")} />
+        <Image style={[styles.key_button, styles.key_3]} source={require("./images/3.png")} />
+        <Image style={[styles.key_button, styles.key_4]} source={require("./images/4.png")} />
+        <Image style={[styles.key_button, styles.key_5]} source={require("./images/5.png")} />
+        <Image style={[styles.key_button, styles.key_6]} source={require("./images/6.png")} />
+        <Image style={[styles.key_button, styles.key_7]} source={require("./images/7.png")} />
+        <Image style={[styles.key_button, styles.key_8]} source={require("./images/8.png")} />
+        <Image style={[styles.key_button, styles.key_9]} source={require("./images/9.png")} />
+        <Image style={[styles.key_button, styles.key_0]} source={require("./images/0.png")} /> */}
+      </View>
+    </SafeAreaView >
+  );
+};
+
+export default Verification;
